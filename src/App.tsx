68877caa62bd29870fcd2511709a505a69a54666
DrummerHead <drummerhead@gmail.com>
2023-07-16 02:41:47 +0300

    Shows two random pics

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,24 @@ interface Pic {
   path: string;
 }
 interface PicResponse {
-  data: Pic[];
+  data: [Pic, ...Pic[]];
   message: 'success';
 }
 
+const randInt = (lessthan: number): number =>
+  Math.floor(Math.random() * lessthan);
+const getRandItem = <T,>(array: T[]): T => array[randInt(array.length)];
+
 function App() {
-  const [pics, setPics] = useState<Pic[]>([]);
+  const [pics, setPics] = useState<Pic[]>([
+    { picId: 0, path: '' },
+    { picId: 1, path: '' },
+  ]);
+  const a: Pic = getRandItem(pics);
+  let b: Pic = getRandItem(pics);
+  while (a === b) {
+    b = getRandItem(pics);
+  }
 
   useEffect(() => {
     axios
@@ -28,9 +40,10 @@ function App() {
 
   return (
     <div>
-      {pics.map((pic) => (
-        <img src={`/pics/${pic.path}`} alt={pic.path} />
-      ))}
+      <div className="mainCompare">
+        <img src={`/pics/${a.path}`} className="a" alt={a.path} />
+        <img src={`/pics/${b.path}`} className="b" alt={b.path} />
+      </div>
     </div>
   );
 }
