ebf52d1e91e923a929dc915ecadd64d9c73d6ca9
DrummerHead <drummerhead@gmail.com>
2023-07-18 00:32:00 +0300

    Show mock ranking

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,19 @@ interface Pic {
   path: string;
 }
 interface PicResponse {
-  data: [Pic, ...Pic[]];
+  data: Pic[];
+  message: 'success';
+}
+
+interface Rank {
+  rank: number;
+  picId: number;
+  path: string;
+  rankedOn: string;
+}
+
+interface RankResponse {
+  data: Rank[];
   message: 'success';
 }
 
@@ -20,6 +32,7 @@ function App() {
     { picId: 0, path: '' },
     { picId: 1, path: '' },
   ]);
+  const [ranking, setRanking] = useState<Rank[]>([]);
   const a: Pic = getRandItem(pics);
   let b: Pic = getRandItem(pics);
   while (a === b) {
@@ -27,15 +40,43 @@ function App() {
   }
 
   useEffect(() => {
+    // Get all pics
     axios
       .get<PicResponse>('/api/v1/pics')
       .then(function (response) {
         setPics(response.data.data);
-        console.log(response);
+        console.log('/api/v1/pics return:');
+        console.log(response.data.data);
       })
       .catch(function (error) {
         console.log(error);
       });
+
+    // Get all rankings
+    axios
+      .get<RankResponse>('/api/v1/ranking')
+      .then(function (response) {
+        setRanking(response.data.data);
+        console.log('/api/v1/ranking return:');
+        console.log(response.data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const keyHandler = (ev: KeyboardEvent): void => {
+      if (ev.key === 'ArrowLeft') {
+        console.log('a');
+      } else if (ev.key === 'ArrowRight') {
+        console.log('b');
+      }
+    };
+    document.addEventListener('keydown', keyHandler);
+    return () => {
+      document.removeEventListener('keydown', keyHandler);
+    };
   }, []);
 
   return (
@@ -44,6 +85,16 @@ function App() {
         <img src={`/pics/${a.path}`} className="a" alt={a.path} />
         <img src={`/pics/${b.path}`} className="b" alt={b.path} />
       </div>
+      <ol className="ranking">
+        {ranking.length > 0
+          ? ranking.map((rank) => (
+              <li>
+                <span>{rank.rank}</span>
+                <img src={`/pics/${rank.path}`} alt={rank.path} />
+              </li>
+            ))
+          : null}
+      </ol>
     </div>
   );
 }
